1e796785fafe76271266bc9995fdff080c01a2e2
codembly <info@codembly.com>
2019-05-26 19:12:53 +0200

    Updated tasks.json

--- basic-data-types/src/main.rs
+++ basic-data-types/src/main.rs
@@ -6,4 +6,7 @@ fn main() {
     // The statements below allow the compiler to infer var is of integer data type
     let var = 10;
     println!("Codemly! The integer number is: {} ", var);
+    // The statements below allow the compiler to infer var is of integer data type
+    let _var1:i32 =  10;
+    println!("Codemly! The integer number is: {} ", _var1);
 }

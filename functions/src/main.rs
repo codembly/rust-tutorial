b3b4bca2dfa51e7262de442da8f85b3e834766b7
codembly <info@codembly.com>
2020-04-12 20:30:54 +0200

    Functions example
    Codembly rust tutorial - functions

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+// Functions in RUST
+// Codebly - Rust Tutorial
+
+//Function definition
+fn sum_integers (x:i32, y:i32) -> i32
+{
+    // explicit value return format
+    //return x+y; 
+    //Tail return expression format
+    x+y
 }
+
+fn main() {
+    let var1 = 10;
+    let var2 = 5;
+    let sum = sum_integers(var1,var2);
+    // Print var1, var2 and their sum values to the console
+    println!("The sum of {} and {} is {}", var1, var2, sum);
+}
+

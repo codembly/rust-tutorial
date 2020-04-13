e5e6809f7ef6b52e1931fe61115af4572e83b004
codembly <info@codembly.com>
2020-04-13 17:47:25 +0200

    modules

--- modules/src/codemly_mod.rs
+++
@@ -1,6 +0,0 @@
-// Rust library modules - codembly_mod.rs
-// Codebly - Rust Tutorial
-// A module named `codembly`
-pub fn print_hello() {
-    println!("Hello, world!");
-}
--- modules/src/main.rs
+++
@@ -1,6 +0,0 @@
-// Rust library modules - main.rs
-// Codebly - Rust Tutorial
-mod codembly_mod;
-fn main() {
-    codembly_mod::print_hello();
-}
